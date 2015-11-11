597c0f35b72f4849482d19f7bf5c336fe43527cd
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-11 15:40:49 -0800

    post and get functional

--- matthew_ringel/routes/cryptid_routes.js
+++ matthew_ringel/routes/cryptid_routes.js
@@ -3,4 +3,31 @@ var bodyParser = require('body-parser');
 var Cryptid = require(__dirname + '/../models/cryptid');
 var handleError = require(__dirname + '/../lib/handleServerError');
 
-var bearsRouter = module.exports = exports = express.Router();
+var cryptoRouter = module.exports = exports = express.Router();
+
+cryptoRouter.post('/cryptids', bodyParser.json(), function(req, res) {
+  var newCryptid = new Cryptid(req.body);
+  newCryptid.save(function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+cryptoRouter.get('/cryptids', function(req, res) {
+  Cryptid.find({}, function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+cryptoRouter.put('/cryptids/:id',bodyParser.json(), function(req, res) {
+  var cryptidData = req.body;
+  delete cryptidData._id;
+  Cryptid.update({id: req.params.id}, cryptidData, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'Cryptid updated.'});
+  });
+});
--- matthew_ringel/server.js
+++ matthew_ringel/server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 var cryptoRouter = require(__dirname + '/routes/cryptid_routes');
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/cryptozoo');
+mongoose.connect('mongodb://localhost/cryptozoo');
 
 app.use('/api', cryptoRouter);
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('server up');
+app.listen(3000, function() {
+  console.log('server listening on port 3000');
 });
