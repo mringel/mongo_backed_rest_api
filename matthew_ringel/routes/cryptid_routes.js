db892d03d5ef932fab7dcde01971a23850f1ef78
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-11 16:49:02 -0800

    basic REST functions done

--- matthew_ringel/routes/cryptid_routes.js
+++ matthew_ringel/routes/cryptid_routes.js
@@ -22,12 +22,21 @@ cryptoRouter.get('/cryptids', function(req, res) {
   });
 });
 
-cryptoRouter.put('/cryptids/:id',bodyParser.json(), function(req, res) {
+cryptoRouter.put('/cryptids/:name', bodyParser.json(), function(req, res) {
   var cryptidData = req.body;
-  delete cryptidData._id;
-  Cryptid.update({id: req.params.id}, cryptidData, function(err) {
+  // delete cryptidData.name;
+  Cryptid.update({name: req.params.name}, cryptidData, function(err) {
     if (err) return handleError(err, res);
 
     res.json({msg: 'Cryptid updated.'});
+  });
+});
+
+cryptoRouter.delete('/cryptids/:name', function(req, res) {
+  Cryptid.remove({name: req.params.name}, function(err) {
+    if (err) return handleError(err, res);
+
+    var msg = req.params.name + ' deleted from database';
+    res.json({msg: msg});
   });
 });
